8a62580515b416ac5ea37c66480c30b233eca357
Val-spire <vaaszschelle@gmail.com>
2026-03-09 19:11:25 +0800

    Mon 03/09/2026 19:11:25.86

--- start.bat
+++ start.bat
@@ -5,5 +5,5 @@ git commit -m "%date% %time%"
 git pull origin main
 
 echo Starting Minecraft server...
-java -Xms16G -Xmx16G -jar fabric-server-launch.jar 
+java -Xms8G -Xmx8G -jar fabric-server-launch.jar 
 pause
